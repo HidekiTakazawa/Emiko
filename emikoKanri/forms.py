932fe22f05f0dcfb7075f3b916b5dccfbd8066fd
HidekiTakazawa <ht0428a30@gmail.com>
2019-12-15 17:19:04 +0900

    forms.py変更

--- emikoKanri/forms.py
+++ emikoKanri/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import ModelForm, Textarea
 
 from .models import NurseRecord
 
@@ -7,3 +8,7 @@ class NurseRecordForm(forms.ModelForm):
     class Meta:
         model = NurseRecord
         fields = ('nurse_day', 'urine_time', 'urine_volume', 'memo')
+        widgets = {
+            'memo': Textarea(attrs={'cols': 50, 'rows': 4}),
+        }
+        # 'memo': Textarea(attrs={'cols': 80, 'rows': 20}),
