3e7ab2a133d27a0bf4ea075a64e868f9171bc71d
HidekiTakazawa <ht0428a30@gmail.com>
2020-04-04 15:06:42 +0900

    ページング機能追加、table横幅　拡張

--- emikoKanri/views.py
+++ emikoKanri/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
@@ -5,9 +6,34 @@ from django.utils import timezone
 from .models import NurseRecord
 from .forms import NurseRecordForm
 # Create your views here.
+def paginate_queryset(request, queryset, count):
+    """Pageオブジェクトを返す。
+
+    ページングしたい場合に利用してください。
+
+    countは、1ページに表示する件数です。
+    返却するPgaeオブジェクトは、以下のような感じで使えます。
+
+        {% if page_obj.has_previous %}
+          <a href="?page={{ page_obj.previous_page_number }}">Prev</a>
+        {% endif %}
+
+    また、page_obj.object_list で、count件数分の絞り込まれたquerysetが取得できます。
+
+    """
+    paginator = Paginator(queryset, count)
+    page = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    return page_obj
 def nurse_list(request):
     nurses = NurseRecord.objects.all().order_by('-nurse_day')
-    return render(request, 'emikoKanri/nurse_list.html', {'nurses':nurses})
+    page_obj = paginate_queryset(request, nurses, 10)
+    return render(request, 'emikoKanri/nurse_list.html', {'page_obj':page_obj})
 def nurse_detail(request, pk):
     nurse = get_object_or_404(NurseRecord, pk=pk)
     return render(request, 'emikoKanri/nurse_detail.html', {'nurse': nurse})
