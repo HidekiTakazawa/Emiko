fb2620ab22bb3c63a140b0248e01eea5662b586b
HidekiTakazawa <ht0428a30@gmail.com>
2020-04-05 17:35:52 +0900

    尿記録メモ　200から500拡張

--- emikoKanri/models.py
+++ emikoKanri/models.py
@@ -7,7 +7,7 @@ class NurseRecord(models.Model):
     nurse_day = models.DateField()
     urine_time = models.CharField(max_length=5, null=True)
     urine_volume = models.PositiveIntegerField(null=True)
-    memo = models.CharField(max_length=200, null=True)
+    memo = models.CharField(max_length=500, null=True)
     def __str__(self):
         return self.memo
 
